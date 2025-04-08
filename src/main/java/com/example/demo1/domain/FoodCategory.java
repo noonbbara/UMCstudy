dd5c45e903572a2e3c6f51507b5a819a8feda628
noonbbara <ysys1226@naver.com>
2025-04-08 17:09:13 +0900

    ✨ feat: 엔티티 작성 및 테이블 생성

---
+++ src/main/java/com/example/demo1/domain/FoodCategory.java
@@ -0,0 +1,22 @@
+package com.example.demo1.domain;
+
+import com.example.demo1.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 15)
+    private String name;
+
+
+}
---
+++ src/main/java/com/example/demo1/domain/ReviewImage.java
@@ -0,0 +1,25 @@
+package com.example.demo1.domain;
+
+import com.example.demo1.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReviewImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column(columnDefinition = "TEXT")
+    private String imageUrl;
+}
